fce8f4926af15ae3d13e2e317dc97648fe176bba
Colin Edwards <ColinJhEdwards@gmail.com>
2022-05-01 19:37:48 -0700

    created fetch function for retrieving products from api, and some if statements for when data is loading or an  error is thrown

---
+++ src/components/Item.tsx
@@ -0,0 +1,12 @@
+import Button from "@material-ui/core/Button";
+// types
+import { CartItemType } from "../App";
+import styled from "styled-components";
+
+function Item() {
+  return <div>Item</div>;
+}
+
+const Wrapper = styled.div``;
+
+export default Item;
